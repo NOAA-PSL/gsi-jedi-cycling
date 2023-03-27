4c1ef5713c7c8008db16d4becd326adf639d650a
Wei Huang <wei.huang@noaa.gov>
2023-03-26 20:50:44 -0500

    add

--- visual-tools/plot-jedi-gsi-diag.py
+++ visual-tools/plot-jedi-gsi-diag.py
@@ -587,7 +587,8 @@ class Plot_JEDI_GSI_Diag():
    #  #plt.xlim(2.2,3.4)
    #Wind rms min: 3.0864446.2, max:   6.84
    #plt.xlim(3.25,5.5)
-    plt.xlim(3.25,5.75)
+   #plt.xlim(3.25,5.75)
+    plt.xlim(4.75,10.75)
     plt.ylim(self.levbot,self.levtop)
     plt.grid(True)
 
@@ -614,7 +615,8 @@ class Plot_JEDI_GSI_Diag():
    #plt.xlim(0.75,2.25)       
    #Temp rms min: 1.2050896.2, max:   4.07
    #plt.xlim(1.0,2.75)       
-    plt.xlim(0.75,2.50)       
+   #plt.xlim(0.75,2.50)       
+    plt.xlim(1.75,3.25)       
     plt.ylim(self.levbot,self.levtop)
     plt.grid(True)
     plt.legend(loc=0)
